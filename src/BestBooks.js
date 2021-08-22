3f26d2d179a742302ace54ddd5214302ec3662fd
Moha-AlHanbali <Moha.AlHanbali@gmail.com>
2021-08-22 14:58:21 +0300

    finished class11

--- src/BestBooks.js
+++ src/BestBooks.js
@@ -8,9 +8,19 @@ class MyFavoriteBooks extends React.Component {
     return(
       <Jumbotron>
         <h1>My Favorite Books</h1>
+        
         <p>
           This is a collection of my favorite books
         </p>
+
+        <ul>
+          <li>Book 1</li>
+          <li>Book 2</li>
+          <li>Book 3</li>
+          <li>Book 4</li>
+          <li>Book 5</li>
+        </ul>
+
       </Jumbotron>
     )
   }
--- src/Login.js
+++ src/Login.js
@@ -2,10 +2,13 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Card from 'react-bootstrap/Card';
 import './Login.css';
+import LoginButton from './LoginButton';
+import { withAuth0 } from '@auth0/auth0-react';
 
 class Login extends React.Component {
   render() {
-    return(
+    const { isAuthenticated } = this.props.auth0;
+    return !isAuthenticated &&(
       <Card style={{ width: '18rem' }}>
         <Card.Body>
           <Card.Title>Log In</Card.Title>
@@ -13,10 +16,13 @@ class Login extends React.Component {
             Click Below to Log In
           </Card.Text>
           {/* TODO: add a `LoginButton` component here that will log the user in with Auth0 */}
+          
+            <LoginButton />
+          
         </Card.Body>
       </Card>
     )
   }
 }
 
-export default Login;
+export default withAuth0(Login);
